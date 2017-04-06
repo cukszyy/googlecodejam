83f0aee07f787231f2ebfe23486c9deabf4f06ed
cukszyy <lucas.portellasantos@gmail.com>
2017-04-06 00:07:50 -0300

    Reverse Words - 2010 Qualification Round

--- reverse-words/reverseWords.java
+++ reverse-words/reverseWords.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+public class ReverseWords {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		// Number of lines. nextLine returns the number of lines on a String.
+		int nLines =  Integer.valueOf(sc.nextLine());
+
+		for (int i = 1; i <= nLines ; i++) {
+
+			// Gets a full line
+			String line = sc.nextLine();
+			
+			// Lines splitted on spaces.
+			String[] words = line.split(" ");
+
+			// This string have all the words reversed
+			String reversedLine = "";
+
+			// Since we splitted the spaces, we have to add them again.
+			for (int j = words.length-1; j >= 0; j--) {
+				reversedLine += " " + words[j];
+			}
+			
+			System.out.println("Case # " + i + ":" + reversedLine);
+		}
+	}
+}
